695986aaeeb4ea2f23689ad2a1b47569540fe2b7
sergey <vasilevich_s@i.ua>
2021-04-24 19:42:48 +0300

    commit 6

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
-require("./websocket");
+//require("./websocket");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
